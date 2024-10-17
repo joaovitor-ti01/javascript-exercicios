5ef7fad6efec9f2e42a436b566dc9cfd2a6bf423
João Vitor Lima <joaolimacaldeira.ti@gmail.com>
2024-10-17 17:48:24 -0300

    desafio01+vetores

---
+++ desafio-01-variaveis/desafio1.js
@@ -0,0 +1,11 @@
+let valorCombustivel = 6.4;
+
+let gastoPorKm = 40;
+
+let distancia = 60;
+
+let litrosConsumidos = (distancia/gastoPorKm);
+
+let gastoTotal = (litrosConsumidos*valorCombustivel);
+
+console.log(gastoTotal);
