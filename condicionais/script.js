b651408d32d8b71fa5f225f7e0f32ea3df19507a
João Vitor Lima <joaolimacaldeira.ti@gmail.com>
2024-10-24 18:41:23 -0300

    add condicionais folder

---
+++ condicionais/script.js
@@ -0,0 +1,38 @@
+// let numero = 8;
+// let testarSePar = ( numero % 2 === 0)
+
+// if( testarSePar ) {
+//     console.log("numero é par ")
+// }
+
+// else {
+//     console.log("numero é impar")
+// }
+
+// analisar se a pessoa é maior de idade
+// let minhaIdade = 17
+// let sabersemaior =  ( minhaidade >= 18)
+
+// if ( sabersemaior) {
+//     console.log("tu é maior")
+// }
+// else {
+//     console.log("tu é menor")
+// }
+
+//
+let carro1 = {
+    marca: "GM",
+    modelo: "Corsa",
+    ano: "2006",
+    cor: "Preta",
+}
+ 
+//se o carro for mais novo que 2010 e na cor preta: comprar 
+
+if(carro1.ano >= 2010 && carro1.cor === "preta"){
+    console.log("comprar")
+}
+else{
+    console.log('Não comprar')
+}
--- vetores-e-objetos/script.js
+++ vetores-e-objetos/script.js
@@ -1,2 +1,33 @@
-let frutas = ["abacaxi", "manga","banana","morango","abacate","bergamota"];
-console.log(frutas)
+// let frutas = ["abacaxi", "manga","banana","morango","abacate","bergamota"];
+//console.log(frutas)
+
+
+// let times = [];
+
+// // push adiciona itens ao final do array
+// times.push("São Paulo")
+// times.push("Flamengo")
+// times.push("Corinthians")
+// times.push("Bahia")
+
+// //pop remove o ultimo item do array
+// times.pop()
+
+// // shift remove o primeiro item do array
+// times.shift()
+
+// //unshift adiciona item no início do array
+// times.unshift("Fortaleza")
+
+// console.log(times[0])
+
+//objetos
+let carro1 = {
+    marca: "GM",
+    modelo: "Corsa",
+    ano: "2006",
+    cor: "Vermelha",
+}
+carro1.combustivel = "flex"
+
+console.log(carro1)
